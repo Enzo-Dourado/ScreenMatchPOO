61517172737047282dddde551b31a30981a325ad
Enzo Dourado <enzodourado.cripto@gmail.com>
2026-03-25 16:08:46 -0300

    Tratando outras exceções e adicionando uma solução para uma delas

--- br/com/testesalura/screenmatch/principal/PrincipalComBusca.java
+++ br/com/testesalura/screenmatch/principal/PrincipalComBusca.java
@@ -18,32 +18,35 @@ public class PrincipalComBusca {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o nome do filme que você quer pesquisar:");
         var buscar = sc.nextLine();
-        String url = "http://www.omdbapi.com/?t=" +  buscar + "&apikey=addb957a";
-
-
-        // Base para todas as chamadas de APIs
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-
-        TituloOmdb meutitulo = gson.fromJson(json, TituloOmdb.class);
+        String url = "http://www.omdbapi.com/?t=" +  buscar.replace(" ", "+") + "&apikey=addb957a";
 
         try {
-        Titulos meuTituloModificado = new Titulos(meutitulo);
-        System.out.println(meuTituloModificado);
+            // Base para todas as chamadas de APIs
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+
+            TituloOmdb meutitulo = gson.fromJson(json, TituloOmdb.class);
+
+
+            Titulos meuTituloModificado = new Titulos(meutitulo);
+            System.out.println(meuTituloModificado);
 
         } catch (NumberFormatException e) {
             System.out.println("Erro capturado: " + e.getMessage());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro de busca capturado: " + e.getMessage());
         }
 
     }
